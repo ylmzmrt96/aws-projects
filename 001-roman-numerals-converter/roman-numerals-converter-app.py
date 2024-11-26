e8b064be7714ea121a05a06df415983f5176b0a2
mert <yilmazmrto@gmail.com>
2024-11-26 22:25:47 +0300

    001-ROMAN-NUMERALS-CONVERTER

--- 001-roman-numerals-converter/roman-numerals-converter-app.py
+++ 001-roman-numerals-converter/roman-numerals-converter-app.py
@@ -21,7 +21,7 @@ def main_post():
         number = int(alpha)
         if not 0 < number < 4000:
             return render_template('index.html', developer_name='Necati', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='oktay')
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Necati')
     else:
         return render_template('index.html', developer_name='Necati', not_valid=False)
 
